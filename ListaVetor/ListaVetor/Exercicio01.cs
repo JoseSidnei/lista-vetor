7b1a1bbfb88f7b6ee35e4c03fbb878b3eb830c71
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-04-24 14:45:20 -0300

    inicialização do repositório

---
+++ ListaVetor/ListaVetor/Exercicio01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaVetor
+{
+    public class Exercicio01
+    {
+        public Exercicio01()
+        {
+
+
+        }
+    }
+}
---
+++ ListaVetor/ListaVetor/Exercicio02.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaVetor
+{
+    public class Exercicio02
+    {
+        public Exercicio02()
+        {
+
+
+
+        }
+    }
+}

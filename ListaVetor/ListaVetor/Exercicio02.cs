7110d13eb318abe5808ce51820d3ae97b8c35238
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-04-24 16:00:14 -0300

    Criação vetor para guardar 10 nomes

--- ListaVetor/ListaVetor/Exercicio02.cs
+++ ListaVetor/ListaVetor/Exercicio02.cs
@@ -13,6 +13,39 @@ namespace ListaVetor
 
             // 2 -​ Crie um vetor para guardar 10 nomes.
 
+            string[] nomes = new string[10];                 
+           
+                    Console.Write("Nome 01: ");
+                    nomes[0] = Console.ReadLine();
+                    Console.WriteLine("");
+                    Console.Write("Nome 02: ");
+                    nomes[1] = Console.ReadLine();
+                    Console.WriteLine("");
+                    Console.Write("Nome 03: ");
+                    nomes[2] = Console.ReadLine();
+                    Console.WriteLine("");
+                    Console.Write("Nome 04: ");
+                    nomes[3] = Console.ReadLine();
+                    Console.WriteLine("");
+                    Console.Write("Nome 05: ");
+                    nomes[4] = Console.ReadLine();
+                    Console.WriteLine("");
+                    Console.Write("Nome 06: ");
+                    nomes[5] = Console.ReadLine();
+                    Console.WriteLine("");
+                    Console.Write("Nome 07: ");
+                    nomes[6] = Console.ReadLine();
+                    Console.WriteLine("");
+                    Console.Write("Nome 08: ");
+                    nomes[7] = Console.ReadLine();
+                    Console.WriteLine("");
+                    Console.Write("Nome 09: ");
+                    nomes[8] = Console.ReadLine();
+                    Console.WriteLine("");
+                    Console.Write("Nome 10: ");
+                    nomes[9] = Console.ReadLine();
+                    Console.WriteLine("");                                                              
+
         }
     }
 }
--- ListaVetor/ListaVetor/Principal.cs
+++ ListaVetor/ListaVetor/Principal.cs
@@ -10,8 +10,8 @@ namespace ListaVetor
     {
         static void Main(string[] args)
         {
-             new Exercicio01();
-            // new Exercicio02();
+            // new Exercicio01();
+             new Exercicio02();
             // new Exercicio03();
 
 

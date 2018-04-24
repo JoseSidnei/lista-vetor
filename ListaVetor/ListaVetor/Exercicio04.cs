c538aefbe1cac92e658f3bcff61fb53a7f394dd4
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-04-24 16:31:53 -0300

    Criação vetor para armazenar 4 notas e realizar a media

--- ListaVetor/ListaVetor/Exercicio04.cs
+++ ListaVetor/ListaVetor/Exercicio04.cs
@@ -15,7 +15,26 @@ namespace ListaVetor
            // Solicite para o usuário o nome e as quatro notas, armazenando as notas em um
            // vetor, realize a média destas notas e apresente.
 
+           
+            string[] nome = new string[0];
+            double[] notas = new double[4];
 
+            Console.Write("Nome: ");
+            nome[0] = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Nota 01: ");
+            notas[0] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nota 02: ");
+            notas[1] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nota 03: ");
+            notas[2] = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nota 04: ");
+            notas[3] = Convert.ToDouble(Console.ReadLine());
+
+            double media = (notas[0] + notas[1] + notas[2] + notas[3] + notas[4]) / 4;
+
+            Console.WriteLine("Média das notas: " + media);
+            
 
 
         }
